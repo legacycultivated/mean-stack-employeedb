00df53d7fddc892f5659ed16d4cde0d6e8a7377f
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2023-01-30 01:04:35 -0800

    create employees list and form

---
+++ client/src/app/employee-form/employee-form.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-form',
+  template: `
+    <p>
+      employee-form works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class EmployeeFormComponent {
+
+}
